0a9b8d9ea666ed58f6e2dd608810c2662e36d6f7
Teerth Patel <teerth1205@gmail.com>
2020-03-06 00:05:57 +0000

    Automatically backed up by Learn

---
+++ app/models/hero.rb
@@ -0,0 +1,17 @@
+class Hero 
+  attr_accessor :name, :power, :bio
+  @@all = []
+  
+  def initialize(hash)
+    @name = hash[:name]
+    @power = hash[:power]
+    @bio = hash[:bio]
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  
+end 
